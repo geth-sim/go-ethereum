1ced2974abff4bdf2a754b34de54e682ab25ed75
jmlee <junmo.lee@snu.ac.kr>
2025-11-27 07:34:22 +0000

    minor fix

--- simulator/evmsim/evm_simulator.go
+++ simulator/evmsim/evm_simulator.go
@@ -754,9 +754,9 @@ func connHandler(conn net.Conn) {
 					"( mode:", common.GetSimulationTypeName(), common.ModifyHashMethod, "/ port:", ServerPort, ")",
 					"\n  current state root: ", currentStateRoot.Hex(),
 					"\n  sub state root:     ", simBlock.SubStateRoot.Hex(),
-					"\n  mainnet header.Root:", header.Root.Hex(),
-					"\n  executed txArgs len:", len(txArgsList),
-					"\n  restored accounts num:", simBlock.AccountRestoreNum, "/ total:", totalRestoreNum,
+					// "\n  mainnet header.Root:", header.Root.Hex(),
+					// "\n  executed txArgs len:", len(txArgsList),
+					// "\n  restored accounts num:", simBlock.AccountRestoreNum, "/ total:", totalRestoreNum,
 				)
 				if common.SimulationMode == common.EthereumMode && currentStateRoot != header.Root {
 					// fmt.Println("ERR: executeTransactionArgsList: Ethereum state not match")
@@ -849,47 +849,51 @@ func connHandler(conn net.Conn) {
 				// to measure dirty stats per block
 				// common.ClearDirtyStats()
 
-				hashedNodeNum := 0
-				for _, v := range common.ModifiedChildNum {
-					hashedNodeNum += v
-				}
-				if common.HashedFullNodeNum != hashedNodeNum {
-					fmt.Println("ERROR: common.HashedFullNodeNum != hashedNodeNum")
-					fmt.Println("  common.HashedFullNodeNum:", common.HashedFullNodeNum)
-					fmt.Println("  hashedNodeNum:", hashedNodeNum)
-					os.Exit(1)
-				}
-
-				if currentBlockNum % saveLevelDBStatsEpoch == 0 {
-					f, err := os.OpenFile("additional_node_stats_log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						panic(err)
+				if common.MeasureChildStats {
+					// check child stats validity
+					hashedNodeNum := 0
+					for _, v := range common.ModifiedChildNum {
+						hashedNodeNum += v
 					}
-					defer f.Close()
-
-					fmt.Fprintf(f, "\nCurrent Block Number: %d\n"+
-						"HashedFullNodeNum: %d\n"+
-						"  nil   childs: %d -> %.2f%%\n"+
-						"  clean childs: %d -> %.2f%%\n"+
-						"  dirty childs: %d -> %.2f%%\n",
-						currentBlockNum,
-						common.HashedFullNodeNum,
-						common.NilChildNum,   float64(common.NilChildNum)*100/float64(common.HashedFullNodeNum)/16,
-						common.CleanChildNum, float64(common.CleanChildNum)*100/float64(common.HashedFullNodeNum)/16,
-						common.DirtyChildNum, float64(common.DirtyChildNum)*100/float64(common.HashedFullNodeNum)/16,
-					)
-
-					// Log ModifiedChildNum values
-					fmt.Fprintf(f, "common.ModifiedChildNum:\n")
-					for x, y := range common.ModifiedChildNum {
-						percent := float64(y) * 100 / float64(hashedNodeNum)
-						fmt.Fprintf(f, "  [%2d]\t= %6d\t-> %6.2f%%\n", x, y, percent)
+					if common.HashedFullNodeNum != hashedNodeNum {
+						fmt.Println("ERROR: common.HashedFullNodeNum != hashedNodeNum")
+						fmt.Println("  common.HashedFullNodeNum:", common.HashedFullNodeNum)
+						fmt.Println("  hashedNodeNum:", hashedNodeNum)
+						os.Exit(1)
 					}
 
-					fmt.Fprintf(f, "\nWrittenTrieNodeNum: %d\n", common.WrittenTrieNodeNum)					
-					fmt.Fprintf(f, "  HashedLeafNodeNum: %d\n", common.HashedLeafNodeNum)
-					fmt.Fprintf(f, "  HashedShortNodeNum: %d\n", common.HashedShortNodeNum)
-					fmt.Fprintf(f, "  HashedFullNodeNum: %d\n", common.HashedFullNodeNum)
+					// save child stats
+					if currentBlockNum % saveLevelDBStatsEpoch == 0 {
+						f, err := os.OpenFile("additional_node_stats.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+						if err != nil {
+							panic(err)
+						}
+						defer f.Close()
+	
+						fmt.Fprintf(f, "\nCurrent Block Number: %d\n"+
+							"HashedFullNodeNum: %d\n"+
+							"  nil   childs: %d -> %.2f%%\n"+
+							"  clean childs: %d -> %.2f%%\n"+
+							"  dirty childs: %d -> %.2f%%\n",
+							currentBlockNum,
+							common.HashedFullNodeNum,
+							common.NilChildNum,   float64(common.NilChildNum)*100/float64(common.HashedFullNodeNum)/16,
+							common.CleanChildNum, float64(common.CleanChildNum)*100/float64(common.HashedFullNodeNum)/16,
+							common.DirtyChildNum, float64(common.DirtyChildNum)*100/float64(common.HashedFullNodeNum)/16,
+						)
+	
+						// Log ModifiedChildNum values
+						fmt.Fprintf(f, "common.ModifiedChildNum:\n")
+						for x, y := range common.ModifiedChildNum {
+							percent := float64(y) * 100 / float64(hashedNodeNum)
+							fmt.Fprintf(f, "  [%2d]\t= %6d\t-> %6.2f%%\n", x, y, percent)
+						}
+	
+						fmt.Fprintf(f, "\nWrittenTrieNodeNum: %d\n", common.WrittenTrieNodeNum)					
+						fmt.Fprintf(f, "  HashedLeafNodeNum: %d\n", common.HashedLeafNodeNum)
+						fmt.Fprintf(f, "  HashedShortNodeNum: %d\n", common.HashedShortNodeNum)
+						fmt.Fprintf(f, "  HashedFullNodeNum: %d\n", common.HashedFullNodeNum)
+					}
 				}
 
 				//
