8e82734cff13abcfb2516309859941636d2e3d73
jmlee <junmo.lee@snu.ac.kr>
2026-04-03 13:43:15 +0900

    set version wraparound

--- trie/hasher.go
+++ trie/hasher.go
@@ -37,6 +37,10 @@ var (
 
 func SetCurrentBlockNum(blockNum uint64) {
 	CurrentBlockNum = blockNum
+
+	// for testing TH's performance when version num is rotating
+	// CurrentBlockNum %= 65535
+	// CurrentBlockNum %= 1048575
 }
 
 // hasher is a type used for the trie Hash operation. A hasher has some
